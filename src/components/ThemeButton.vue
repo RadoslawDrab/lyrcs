040b635439e80834ececb81ffc8b4bbe8e4946b8
Radosław Drab <radoslaw.drab03@gmail.com>
2025-01-05 21:29:32 +0100

    added theme button

---
+++ src/components/ThemeButton.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import {watch} from "vue";
+import {PhMoon, PhSun} from "@phosphor-icons/vue";
+
+export type Theme = 'dark' | 'light'
+
+const props = withDefaults(defineProps<{size?: number}>(), {
+  size: 24
+})
+const currentTheme = defineModel<Theme>({default: 'light'})
+
+function set(theme: Theme) {
+  const htmlEl = document.documentElement
+  if (!htmlEl) return
+  htmlEl.classList.remove('dark')
+  htmlEl.classList.remove('light')
+  htmlEl.classList.add(theme)
+}
+function toggle() {
+  currentTheme.value = currentTheme.value == 'light' ? 'dark' : 'light'
+}
+watch(currentTheme, set)
+</script>
+
+<template>
+  <button class="btn-outline btn-contrast p-1" @click="toggle" :data-theme="currentTheme" aria-label="Theme mode toggle button">
+    <PhSun v-if="currentTheme === 'light'" :size="props.size" />
+    <PhMoon v-if="currentTheme === 'dark'" :size="props.size" />
+  </button>
+</template>
