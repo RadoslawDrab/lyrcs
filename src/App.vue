4fbc432adbf02bdf7ba00e09ed4e7df3bb7a8975
Radosław Drab <radoslaw.drab03@gmail.com>
2025-01-06 00:04:15 +0100

    fixed layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+
+
+
+
+
 </script>
 
 <template>
+  <Header />
+  <Main />
 </template>
-
-<style scoped>
-</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,8 +3,11 @@
 import FileInput from "./FileInput.vue";
 import {ref} from "vue";
 import {Line} from "../types/lyrics";
+import AudioControls from "./AudioControls.vue";
+import {PhPlus, PhX} from "@phosphor-icons/vue";
 
 const audioFile = ref<{type: string, url: string} | null>(null);
+const audioElement = ref<HTMLAudioElement | null>(null);
 const lines = ref<Line[]>([])
 
 function createLine(index: number) {
@@ -25,38 +28,29 @@ function onFilesLoad(files: File[]) {
 </script>
 
 <template>
-  <form @submit.prevent="submit">
-    <FileInput accept="audio/.*" @filesChange="onFilesLoad" />
-    <div v-if="audioFile">
-      <audio id="audio" controls>
-        <source :type="audioFile.type" :src="audioFile.url" />
-      </audio>
-      <ul>
-        <li v-for="line in lines" :key="line.index" class="line">
-          <label for="start-input">Start</label>
-          <input min="0" name="start-input" id="start-input" v-model.number="line.start" pattern="[0-9]{2,}:[0-9]{2}:[0-9]{2}" />
-          <label for="end-input">End</label>
-          <input min="0" name="end-input" id="end-input" v-model="line.end" pattern="[0-9]{2,}:[0-9]{2}:[0-9]{2}" />
-          <input name="text-input" id="text-input" v-model="line.text" />
-          <button @click="() => deleteLine(line.index)">Delete</button>
+  <main class="max-w-2xl mx-auto px-2">
+    <form @submit.prevent="submit">
+      <FileInput accept="audio/.*" @filesChange="onFilesLoad" />
+    </form>
+      <div v-if="audioFile" class="mt-6 p-2 border-full">
+        <AudioControls v-model="audioFile" />
+      </div>
+      <ul class="flex flex-col gap-3 mt-4 border-full p-2">
+        <li class="group" v-for="line in lines" :key="line.index">
+          <button class="btn-outline btn-danger p-1" @click="() => deleteLine(line.index)">
+            <PhX :size="24" />
+          </button>
+          <span class="p-2">00:00:000</span>
+          <input class="w-full input p-2" v-model="line.text" />
+          <span class="p-2">00:00:000</span>
+        </li>
+        <li>
+          <button class="btn btn-success p-2 w-full flex gap-2 items-center justify-center" @click="() => createLine(lines.length)">
+            <PhPlus :size="24" weight="bold" />
+            <span>Add line</span>
+          </button>
         </li>
       </ul>
-      <button @click="() => createLine(lines.length)">Add line</button>
-      <button :disabled="lines.length <= 0" type="submit">Submit</button>
-    </div>
-  </form>
+    <button :disabled="lines.length <= 0" type="submit">Submit</button>
+  </main>
 </template>
-
-<style scoped lang="scss">
-.line {
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-}
-.dropbox {
-  padding: 7rem;
-  border: 1px solid gray;
-  border-radius: 5px;
-  text-align: center;
-}
-</style>
